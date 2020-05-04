fd7c40d31558e4198101dff5de3afb919c123824
devindersingh79904 <devindersingh79904@gmail.com>
2020-05-04 17:06:20 +0530

    ADDed name field ,Added Topping,Added Toast

--- app/src/main/java/com/example/justjava/MainActivity.java
+++ app/src/main/java/com/example/justjava/MainActivity.java
@@ -4,13 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
 
-    int numberOfCoffee = 0;
+    int numberOfCoffee = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,11 +22,27 @@ public class MainActivity extends AppCompatActivity {
 
     public void submitOrder(View view)
     {
-        String message = "Total Price : $" + numberOfCoffee * 5 + "\nThankYou";
+
+
+        CheckBox wippedCreamCheckbox  = (CheckBox) findViewById(R.id.wipped_cream_checkbox_view);
+        boolean hasWippedCream = wippedCreamCheckbox.isChecked();
+
+        CheckBox chocolateCheckbox  = (CheckBox) findViewById(R.id.choclate_checkbox_view);
+        boolean hasChocolate = chocolateCheckbox.isChecked();
+
+        EditText nameEditText = (EditText) findViewById(R.id.name_edit_text);
+        String name = nameEditText.getText().toString();
+
+
+        int price = calclulatePrice(hasWippedCream,hasChocolate);
+
+        String message = createOrderSummery(name,price,hasWippedCream,hasChocolate);
+
+
         displayMessage(message);
     }
 
-    private void dispaly(int number) {
+    private void displayQunatity(int number) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
 
         quantityTextView.setText("" + number);
@@ -31,27 +50,60 @@ public class MainActivity extends AppCompatActivity {
 
     public void incrementQuantity(View view)
     {
+        if(numberOfCoffee == 100)
+        {
+            Toast.makeText(getApplicationContext(),"Length cannot be graeter Than 100",Toast.LENGTH_SHORT).show();
+            return;
+        }
         numberOfCoffee++;
-        dispaly(numberOfCoffee);
-        dispalyPrice(numberOfCoffee);
-
+        displayQunatity(numberOfCoffee);
     }
+
 
     public void decrementQuantity(View view)
     {
+        if(numberOfCoffee == 1){
+            Toast.makeText(getApplicationContext(),"Length cannot be negtive",Toast.LENGTH_SHORT).show();
+            return;
+        }
+
         numberOfCoffee--;
-        dispaly(numberOfCoffee);
-        dispalyPrice(numberOfCoffee);
+        displayQunatity(numberOfCoffee);
+
     }
-    private void dispalyPrice(int number) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number * 5 ));
+
+
+    private int calclulatePrice(boolean hasWippedCream,boolean hasChocolate)
+    {
+        int price = 5;
+
+        if(hasWippedCream)
+            price++;
+
+        if(hasChocolate)
+            price += 2;
+
+        price = numberOfCoffee * price;
+        return price;
     }
+
+    private String createOrderSummery(String name,int price,boolean hasWippedCream,boolean hasChocolate)
+    {
+        String priceMessage = "Name : " + name;
+        priceMessage += "\nQuantity : " + numberOfCoffee;
+        priceMessage += "\nAdd Wipped Cream : " + hasWippedCream;
+        priceMessage += "\nAdd Chocolate : " + hasChocolate;
+        priceMessage += "\ntotal : $" + price;
+        priceMessage += "\nThankYou";
+
+        return priceMessage;
+    }
+
+
 
     private  void displayMessage(String message)
     {
-        TextView pricetextView = (TextView) findViewById(R.id.price_text_view);
-
-        pricetextView.setText(message);
+        TextView orderSummeryView = (TextView) findViewById(R.id.order_summary_text_view);
+        orderSummeryView.setText(message);
     }
 }
